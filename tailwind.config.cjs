810376623cf0ee13e250b00893d0b36219765350
Yusuf Güneş <gunesyusuf838@gmail.com>
2023-01-16 06:06:30 +0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'herobackground': "url('./public/bg-cover.svg')",
+      }
+    },
+  },
+}
